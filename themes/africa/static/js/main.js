0bc62b9464f3aee70a809164a621b6d8f0faf5ef
Ulysses Ferreira Murja <ulyssesmurja@gmail.com>
2018-06-04 00:51:54 -0300

    serialize

--- themes/africa/static/js/main.js
+++ themes/africa/static/js/main.js
@@ -99,7 +99,22 @@ $(".button-submit").click(function() {
     }
 });
 
-
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
 
 
 var $form = $('form#sayhello'),
